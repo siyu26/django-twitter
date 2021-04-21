ad4150aa9c0dc216537a6de17382ab5c95fc5d22
Siyu Peng <siyu@Siyus-Air.home>
2021-04-20 20:22:00 -0400

    user authentication api

--- twitter/urls.py
+++ twitter/urls.py
@@ -20,10 +20,11 @@ from accounts.api import views
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
+router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',
-namespace='rest_framework')),
+                              namespace='rest_framework')),
 ]
